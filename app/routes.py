3d365effbbd7193d8a2b9ab614de47f629fd9dd6
ErdalDomi <erdal.domi2@gmail.com>
2019-01-01 17:26:08 +0100

    integrated all html in repository

--- app/routes.py
+++ app/routes.py
@@ -9,12 +9,14 @@ def index():
 
     return render_template('main.html')
 
+@app.route('/timeline')
+def timeline():
+    return render_template('timeline.html')
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
-        return redirect(url_for('index'))
-    return render_template('login.html',  title='Sign In', form=form)
+@app.route('/medintake')
+def medintake():
+    return render_template('medintake.html')
+
+@app.route('/visualization')
+def visualization():
+    return render_template('visualization.html')
